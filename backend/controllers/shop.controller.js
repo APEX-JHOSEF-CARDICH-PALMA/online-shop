c58277215963859b207ecd6d4d9dc5801724dbc3
Pawel <siwonpawel@gmail.com>
2020-08-25 18:35:27 +0200

    Add endpoint to delete shop

--- backend/controllers/shop.controller.js
+++ backend/controllers/shop.controller.js
@@ -45,3 +45,29 @@ export function findShop(req, res) {
       })
     );
 }
+
+export function deleteShop(req, res) {
+  Shop.forge({ id: req.params.id })
+    .fetch({ require: true })
+    .then((shop) =>
+      shop
+        .destroy()
+        .then(() =>
+          res.json({
+            error: false,
+            data: { message: "Shop deleted successfully." },
+          })
+        )
+        .catch((err) =>
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+            error: true,
+            data: { message: err.message },
+          })
+        )
+    )
+    .catch((err) =>
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        error: err,
+      })
+    );
+}
--- backend/routes/shop.route.js
+++ backend/routes/shop.route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/").get(shopCtrl.findAll);
 router.route("/:id").get(shopCtrl.findShop);
+router.route("/:id").delete(shopCtrl.deleteShop);
 
 export default router;
