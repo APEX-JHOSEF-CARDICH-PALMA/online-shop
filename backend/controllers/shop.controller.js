eda69f44cf27d9692138d8acddd6c2b956847ed5
Pawel <siwonpawel@gmail.com>
2020-08-25 18:58:47 +0200

    Mark shop as inactive

--- backend/controllers/shop.controller.js
+++ backend/controllers/shop.controller.js
@@ -49,9 +49,10 @@ export function findShop(req, res) {
 export function deleteShop(req, res) {
   Shop.forge({ id: req.params.id })
     .fetch({ require: true })
-    .then((shop) =>
+    .then((shop) => {
+      shop.attributes.active = 0;
       shop
-        .destroy()
+        .save()
         .then(() =>
           res.json({
             error: false,
@@ -63,8 +64,8 @@ export function deleteShop(req, res) {
             error: true,
             data: { message: err.message },
           })
-        )
-    )
+        );
+    })
     .catch((err) =>
       res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
         error: err,
